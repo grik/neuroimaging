84b171585dc3cdbb327527dd26ac65683cf8d60e
grik <grik.amu@gmail.com>
2014-10-31 17:18:41 +0000

    Update create_atlas.sh

--- create_atlas.sh
+++ create_atlas.sh
@@ -7,8 +7,8 @@ mkdir atlas
 #To run this fslmerge command you have to run reg2atlas.sh script before. Register all subject's anatomy files to atlas and then you can merge T1w.nii.gz images to create atlas for six subjects.
 fslmerge -t atlas/atlas_init_4D sub001/anatomy/T1w.nii.gz sub002/anatomy/T1w.nii.gz sub003/anatomy/T1w.nii.gz sub004/anatomy/T1w.nii.gz sub005/anatomy/T1w.nii.gz sub006/anatomy/T1w.nii.gz
 
-fslmaths atlas/atlas_init_4D.nii.gz -Tmean atlas_init
+fslmaths atlas/atlas_init_4D.nii.gz -Tmean atlas/atlas_init
 
-fslswapdim atlas/atlas_init -x y z atlas_init_flipped
+fslswapdim atlas/atlas_init -x y z atlas/atlas_init_flipped
 
-fslmaths atlas/atlas_init -add atlas_init_flipped.nii.gz -div 2 atlas_flirt
+fslmaths atlas/atlas_init -add atlas/atlas_init_flipped.nii.gz -div 2 atlas/atlas_flirt
