b329cf3a2e3bb1d30b344d03dbb9ff87dec06f45
grik <grik.amu@gmail.com>
2014-11-15 17:14:25 +0100

    Update reg2atlas.sh

--- reg2atlas.sh
+++ reg2atlas.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-shopt -s expand_aliases
-source ~/.fsl_inte 
 
 lista=$(ls -1 | grep "sub00")
 
@@ -9,7 +7,7 @@ do
 
     cd $i/anatomy
     
-    flirt -in highres001.nii.gz -ref /usr/share/fsl/5.0/data/standard/MNI152_T1_2mm -out T1w -omat T1w.mat -cost mutualinfo -dof 12 -interp spline
+    flirt -in highres001.nii.gz -ref $FSLDIR/data/standard/MNI152_T1_2mm -out T1w -omat T1w.mat -cost mutualinfo -dof 12 -interp spline
     
     cd ../..
 
